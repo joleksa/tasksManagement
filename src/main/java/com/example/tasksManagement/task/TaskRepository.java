ac7bc85b77563780429ca7f3e4e4d84b9d8acdf8
joleksa <jakub.oleksa@gmail.com>
2024-03-06 08:01:19 +0100

    added filter for repository

--- src/main/java/com/example/tasksManagement/task/TaskRepository.java
+++ src/main/java/com/example/tasksManagement/task/TaskRepository.java
@@ -1,6 +1,10 @@
 package com.example.tasksManagement.task;
 
+import com.example.tasksManagement.user.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    Optional<Task> findByAssignedUser(AppUser assignedUser);
 }
