fd84533f9db9b296614bb80891575e4482fb9650
joleksa <jakub.oleksa@gmail.com>
2024-03-07 13:00:35 +0100

    FEATURE005: added assigning task to user and responseEntities to endpoints

---
+++ src/main/java/com/example/tasksManagement/Dto/AssignTaskDto.java
@@ -0,0 +1,9 @@
+package com.example.tasksManagement.Dto;
+
+import lombok.Getter;
+
+@Getter
+public class AssignTaskDto {
+    private Long taskId;
+    private Long userId;
+}
--- src/main/java/com/example/tasksManagement/Dto/TaskResponseDto.java
+++ src/main/java/com/example/tasksManagement/Dto/TaskResponseDto.java
@@ -1,14 +1,14 @@
 package com.example.tasksManagement.Dto;
 
+import com.example.tasksManagement.task.taskEnum.TaskStatus;
 import com.example.tasksManagement.task.taskEnum.TaskType;
 import lombok.Getter;
-
+import lombok.Setter;
 
 @Getter
-public class TaskDto {
+@Setter
+public class TaskResponseDto {
     private TaskType taskType;
     private String description;
-    private Long assignedUserId;
-    private Long createdById;
-    private int daysToEnd;
+    private TaskStatus taskStatus;
 }
